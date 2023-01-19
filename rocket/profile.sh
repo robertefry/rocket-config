1bb1aaa6bd5ce7dcf89e266d15df918978a96c07
Robert Fry <inbox@robertfry.xyz>
2023-01-19 20:59:03 +0000

    profile: refactor extract function

--- rocket/profile.sh
+++ rocket/profile.sh
@@ -174,20 +174,22 @@ extract()
 {
     if [ -f $1 ]; then
         case $1 in
-            *.tar.bz2)  tar xjf $@      ;;
-            *.tar.gz)   tar xzf $@      ;;
-            *.bz2)      bunzip2 $@      ;;
-            *.gz)       gunzip $@       ;;
-            *.rar)      unrar x $@      ;;
-            *.tar)      tar xf $@       ;;
-            *.tbz2)     tar xjf $@      ;;
-            *.tgz)      tar xzf $@      ;;
-            *.xz)       unxz $@         ;;
-            *.Z)        uncompress $@   ;;
-            *.zip)      unzip $@        ;;
-            *.7z)       7z x $@         ;;
-            *.deb)      ar x $@         ;;
-            *)      echo "'$1' has an unknown file extension";;
+            *.tar)              tar xf $@       ;;
+            *.tar.bz2|*.tbz2)   tar xjf $@      ;;
+            *.tar.gz|*.tgz)     tar xzf $@      ;;
+            *.tar.xz|*.txz)     tar xf $@       ;;
+
+            *.bz2)              bunzip2 $@      ;;
+            *.gz)               gunzip $@       ;;
+            *.xz)               unxz $@         ;;
+
+            *.rar)              unrar x $@      ;;
+            *.Z)                uncompress $@   ;;
+            *.zip)              unzip $@        ;;
+            *.7z)               7z x $@         ;;
+            *.deb)              ar x $@         ;;
+
+            *)  echo "'$1' has an unknown file extension";;
         esac
     else
         echo "'$1' is not a valid file"
