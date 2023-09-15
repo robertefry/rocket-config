98515aa0d22ffe61f797a34049f3a376812bb940
Robert Fry <inbox@robertfry.xyz>
2023-09-15 22:49:48 +0100

    fix: PS1 not rendering non-successful exit codes

--- resources/home-user/.config/rocket-config/profile.sh
+++ resources/home-user/.config/rocket-config/profile.sh
@@ -12,13 +12,13 @@ subshell()
 
 PS1_prompt_command()
 {
+    printf -v EXIT '%02x' $?    # 2-digit hex exit code
+
     PS1_FMT0="\e[0m"
     [ $(id -u) = 0 ] && PS1_FMT1="\e[0;1;31m"   || PS1_FMT1="\e[0;1;36m"
     [ $(id -u) = 0 ] && PS1_FMT2="\e[0;1;3;31m" || PS1_FMT2="\e[0;1;3;36m"
     [ $(id -u) = 0 ] && PS1_FMT3="\e[0;1;33m"   || PS1_FMT3="\e[0;1;34m"
     [ $(id -u) = 0 ] && PS1_FMTE="\e[0;1;3;33m" || PS1_FMTE="\e[0;1;3;31m"
-
-    printf -v EXIT '%02x' $?    # 2-digit hex exit code
 
     PS1="\[$PS1_FMT1\][\[$PS1_FMT2\]\u\[$PS1_FMT1\]@\H \[$PS1_FMT0\]\W\[$PS1_FMT1\]]"
 
