6244ca544fc84950adb6e4b1d24d8149af27acae
Robert Fry <inbox@robertfry.xyz>
2024-10-03 13:31:16 +0100

    fix: move xdg profile entirely to home-home

--- resources/home-home/.config/rocket-config/profile-home.sh
+++ resources/home-home/.config/rocket-config/profile-home.sh
@@ -3,6 +3,16 @@
 ## XDG DIRECTORY STRUCTURE
 ################################################################################
 
+# DBUS session
+export XDG_RUNTIME_DIR="${XDG_RUNTIME_DIR:-/run/user/$(id -u)}"
+export DBUS_SESSION_BUS_ADDRESS="${DBUS_SESSION_BUS_ADDRESS:-unix:path=$XDG_RUNTIME_DIR/bus}"
+
+# XDG home directories
+export XDG_CACHE_HOME="$HOME/.cache"
+export XDG_CONFIG_HOME="$HOME/.config"
+export XDG_DATA_HOME="$HOME/.local/share"
+export XDG_STATE_HOME="$HOME/.local/state"
+
 # bash
 export HISTFILE="$XDG_STATE_HOME"/bash/history
 # python
--- resources/home-user/.config/rocket-config/profile.sh
+++ resources/home-user/.config/rocket-config/profile.sh
@@ -36,20 +36,6 @@ PS1_prompt_command()
 export PROMPT_COMMAND=PS1_prompt_command
 
 ################################################################################
-## XDG DIRECTORY STRUCTURE
-################################################################################
-
-# DBUS session
-export XDG_RUNTIME_DIR="${XDG_RUNTIME_DIR:-/run/user/$(id -u)}"
-export DBUS_SESSION_BUS_ADDRESS="${DBUS_SESSION_BUS_ADDRESS:-unix:path=$XDG_RUNTIME_DIR/bus}"
-
-# XDG home directories
-export XDG_CACHE_HOME="$HOME/.cache"
-export XDG_CONFIG_HOME="$HOME/.config"
-export XDG_DATA_HOME="$HOME/.local/share"
-export XDG_STATE_HOME="$HOME/.local/state"
-
-################################################################################
 ## ENVIRONMENT VARIABLES
 ################################################################################
 
