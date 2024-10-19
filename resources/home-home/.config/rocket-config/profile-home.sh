64407594e0ac197b25e1f48c28488fa99ef01352
Robert Fry <inbox@robertfry.xyz>
2024-10-19 01:40:38 +0100

    feat: set rust directories

--- resources/home-home/.config/rocket-config/profile-home.sh
+++ resources/home-home/.config/rocket-config/profile-home.sh
@@ -15,6 +15,9 @@ export XDG_STATE_HOME="$HOME/.local/state"
 
 # bash
 export HISTFILE="$XDG_STATE_HOME"/bash/history
+# rust
+export RUSTUP_HOME="$XDG_DATA_HOME"/rustup
+export CARGO_HOME="$XDG_DATA_HOME"/cargo
 # python
 export PYTHONSTARTUP="$HOME"/.pythonrc
 # gnupg
