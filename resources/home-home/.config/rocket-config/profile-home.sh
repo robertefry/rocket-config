0f781e216c2563a72e3a7be5aff6b9d4c0ff069c
Robert Fry <inbox@robertfry.xyz>
2024-10-26 01:40:43 +0100

    fix: append flatpak exports to xdg data dirs

--- resources/home-home/.config/rocket-config/profile-home.sh
+++ resources/home-home/.config/rocket-config/profile-home.sh
@@ -12,6 +12,7 @@ export XDG_CACHE_HOME="$HOME/.cache"
 export XDG_CONFIG_HOME="$HOME/.config"
 export XDG_DATA_HOME="$HOME/.local/share"
 export XDG_STATE_HOME="$HOME/.local/state"
+export XDG_DATA_DIRS="$XDG_DATA_DIRS:/var/lib/flatpak/exports/share"
 
 # bash
 export HISTFILE="$XDG_STATE_HOME"/bash/history
