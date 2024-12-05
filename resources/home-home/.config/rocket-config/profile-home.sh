1510865cd5cb2fd537ada6e1dd42afd37bac5f4c
Robert Fry <inbox@robertfry.xyz>
2024-12-05 19:15:45 +0000

    fix: move home variables to profile-home.sh

--- resources/home-home/.config/rocket-config/profile-home.sh
+++ resources/home-home/.config/rocket-config/profile-home.sh
@@ -62,3 +62,44 @@ then
     export SDL_IM_MODULE=fcitx
     export XMODIFIERS=@im=fcitx
 fi
+
+## Use MangoHUD (Vulcan/OpenGL)
+export MANGOHUD=1
+export MANGOHUD_DLSYM=1
+export ENABLE_VKBASALT=1
+
+## Python
+if command -v python >/dev/null 2>&1
+then
+    alias py=python
+    export PYTHONSTARTUP=~/.pythonrc
+fi
+
+## VSCodium
+if command -v flatpak >/dev/null 2>&1 \
+    && [ "$(flatpak list | grep com.vscodium.codium)" != "" ]
+then
+    alias vscodium='flatpak run com.vscodium.codium'
+fi
+if command -v vscodium >/dev/null 2>&1
+then
+    alias code=vscodium
+fi
+
+## JRE Runtime Options
+export JDK_JAVA_OPTIONS='-Dsun.java2d.opengl=true -Dawt.useSystemAAFontSettings=on -Dswing.aatext=true -Dswing.defaultlaf=com.sun.java.swing.plaf.gtk.GTKLookAndFeel -Dswing.crossplatformlaf=com.sun.java.swing.plaf.gtk.GTKLookAndFeel'
+
+## Youtube Downloader
+if command -v yt-dlp >/dev/null 2>&1; then
+    alias ytdl=yt-dlp
+elif command -v youtube-dl >/dev/null 2>&1; then
+    alias ytdl=youtube-dl
+else
+    echo ytdl: backend missing, consider installing yt-dlp or youtube-dl
+fi
+
+## List Steam Games and IDs
+steamapps()
+{
+    find "$1" -maxdepth 1 -type f -name '*.acf' -exec awk -F '"' '/"appid|name/{ printf $4 "|" } END { print "" }' {} \; | column -t -s '|' | sort -k 2
+}
