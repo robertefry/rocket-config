dfebfb5e176ff03b89ee5e4c0ba1180da834b127
Robert Fry <inbox@robertfry.xyz>
2025-01-14 10:00:33 +0000

    fix: python venv inherit from system packages

--- resources/home-home/.config/rocket-config/profile-home.sh
+++ resources/home-home/.config/rocket-config/profile-home.sh
@@ -82,7 +82,7 @@ if command -v pip3 >/dev/null 2>&1
 then
     if [ ! -r "$XDG_DATA_HOME/rocket-config/venv/bin/activate" ]
     then
-        python3 -m venv "$XDG_DATA_HOME/rocket-config/venv"
+        python3 -m venv --system-site-packages "$XDG_DATA_HOME/rocket-config/venv"
     fi
 
     . "$XDG_DATA_HOME/rocket-config/venv/bin/activate"
