287103720abd44859ef8d4b84fbe65c0209bbbcf
Robert Fry <inbox@robertfry.xyz>
2025-07-17 20:27:24 +0100

    fix: retain keychain for 1 hour

--- resources/.config/environment.d/90-keychain.sh
+++ resources/.config/environment.d/90-keychain.sh
@@ -2,5 +2,5 @@
 if command -v keychain >/dev/null 2>&1
 then
     alias keychain="keychain --absolute --dir "$XDG_RUNTIME_DIR"/keychain"
-    eval $(keychain --eval --quiet --timeout 5)
+    eval $(keychain --eval --quiet --timeout 60)
 fi
