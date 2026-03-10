a80ec15108ebb1425893ca67b13cc727c2a2327b
Robert Fry <inbox@robertfry.xyz>
2026-03-10 12:38:03 +0000

    fix: 90-python.sh improvements

--- resources/.config/environment.d/90-python.sh
+++ resources/.config/environment.d/90-python.sh
@@ -7,11 +7,13 @@ fi
 function py()
 {
     local VENV_DIR="${XDG_DATA_HOME:-$HOME/.local/share}/venv/"
+    local VENV_PYTHON="$VENV_DIR/bin/python3"
 
-    if [ ! -r "$VENV_DIR/bin/activate" ]
+    if [ ! -r "$VENV_PYTHON" ]
     then
+        printf "%s\n" "Creating a new python virtual environment in $VENV_DIR..."
         python3 -m venv "$VENV_DIR"
     fi
 
-    "$VENV_DIR/bin/python3" "$@"
+    "$VENV_PYTHON" "$@"
 }
